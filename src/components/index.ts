43d57e1b84269be258b413b782fddb9e2cd839e8
David92p <dave.panetta92@gmail.com>
2024-04-08 16:51:41 +0200

    Added imports components

---
+++ src/components/index.ts
@@ -0,0 +1,13 @@
+import Header from "./header/Header";
+
+// main
+import Main from "./main/Main";
+import Introduction from "./main/Introduction";
+
+// data search
+import DataSearch from "./dataSearch/DataSearch";
+
+// global components 
+import Button from "./global/Button";
+
+export { Header, Main, Introduction, DataSearch, Button }
