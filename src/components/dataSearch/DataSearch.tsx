c45464bf9006132ac0df412ffbc68af3dd59ef07
David92p <dave.panetta92@gmail.com>
2024-04-08 16:49:51 +0200

    Added component datasearch

---
+++ src/components/dataSearch/DataSearch.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DataSearch:React.FC = () => {
+  return (
+    <div className='bg-slate-100 h-44 border-4 border-mysecondyellow'>
+      GRAPHIC DATA INSERTED HERE
+    </div>
+  )
+}
+
+export default DataSearch
