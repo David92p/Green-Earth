97d60716598401c60ed6388759d1038c025463df
David92p <dave.panetta92@gmail.com>
2024-04-08 16:51:22 +0200

    Added functional components to rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { DataSearch, Header, Main } from "./components"
 
 
 const App:React.FC = () => {
 
+  // useEffect(() => {
+  //   fetch("https://api.v2.emissions-api.org/api/v2/carbonmonoxide/statistics.json?country=it&interval=week&end=2024-04-05&limit=1&offset=0")
+  //   .then(res => {
+  //     const x = res.json()
+  //     return x
+  //   })
+  //   .then(res => {
+  //     console.log(res)
+  //   })
+  // }, [])
+
   return (
-    <>
-      <h1>COMPONENTE APP</h1>
-    </>
+    <div className="flex flex-col bg-firstyellow">
+      <Header />
+      <DataSearch/>
+      <Main/>
+    </div>
   )
 }
 
