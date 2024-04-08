a9bdfeffa08b2c5642b68c2d30a1f83684d2ec2a
David92p <dave.panetta92@gmail.com>
2024-04-08 16:50:34 +0200

    Added Main component

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Introduction from './Introduction'
+
+const Main:React.FC = () => {
+  return (
+    <div className='flex flex-col p-4 bg-myfirstyellow'>
+        <Introduction/>
+    </div>
+  )
+}
+
+export default Main
