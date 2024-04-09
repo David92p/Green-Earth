881cef7a46abb362c49cc1107831d5883ee35218
David92p <dave.panetta92@gmail.com>
2024-04-09 16:27:15 +0200

    Changed year in title and fixed CSS

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,18 +9,18 @@ import img4 from "../../assets/images/header-4.png"
 
 const Header:React.FC = () => {
   return (
-    <div className="flex flex-col 2xl:flex-row bg-myfirstyellow p-4">
+    <div className="flex flex-col 2xl:flex-row 2xl:justify-around bg-myfirstyellow p-4">
       <div className="flex flex-col 2xl:w-1/2 sm:justify-center sm:gap-4">
         <p className="text-mygreen font-mytitlefont text-xl sm:text-2xl font-bold tracking-wider text-center my-4">ENVIRONMENTAL MONITORING</p>
-        <p className="text-mygreen font-mytitlefont text-6xl sm:text-7xl font-bold tracking-wider text-center">Our 2030 Journey</p>
+        <p className="text-mygreen font-mytitlefont text-6xl sm:text-7xl font-bold tracking-wider text-center">Our 2050 Journey</p>
         <Button name={"EXPLORE OUR REPORT"} />
       </div>
       {/* container immages */}
-      <div className="flex flex-col sm:flex-row sm:justify-center 2xl:w-1/2 2xl:flex-wrap">
-        <img src={img1} alt="img1" className="sm:w-1/4 2xl:w-1/2"/>
-        <img src={img2} alt="img2" className="sm:w-1/4 2xl:w-1/2"/>
-        <img src={img4} alt="img2" className="sm:w-1/4 2xl:w-1/2"/>
-        <img src={img3} alt="img3" className="sm:w-1/4 2xl:w-1/2"/>
+      <div className="flex flex-col items-center sm:flex-row 2xl:w-2/5 2xl:flex-wrap">
+        <img src={img1} alt="img1" className="w-4/5 sm:w-1/4 sm:h-52 2xl:w-1/2 2xl:pr-2 2xl:pb-2"/>
+        <img src={img2} alt="img2" className="w-4/5 sm:w-1/4 sm:h-52 2xl:w-1/2 2xl:pb-2"/>
+        <img src={img4} alt="img4" className="w-4/5 sm:w-1/4 sm:h-52 2xl:w-1/2 2xl:pr-2"/>
+        <img src={img3} alt="img3" className="w-4/5 sm:w-1/4 sm:h-52 2xl:w-1/2"/>
       </div>
     </div>
   )
