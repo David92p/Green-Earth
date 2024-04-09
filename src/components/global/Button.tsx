7021686d9d050ed43265b413399166f0b4e57937
David92p <dave.panetta92@gmail.com>
2024-04-09 16:27:31 +0200

    Fixed CSS

--- src/components/global/Button.tsx
+++ src/components/global/Button.tsx
@@ -6,7 +6,7 @@ type ButtonType = {
 
 const Button:React.FC<ButtonType> = ({ name }) => {
   return (
-    <button className="bg-mysecondyellow text-mygreen font-bold text-lg sm:text-2xl tracking-wider w-3/4 sm:w-2/4 mx-auto my-6 py-2 sm:py-4 rounded-3xl">
+    <button className="bg-mysecondyellow text-mygreen font-bold text-md sm:text-2xl tracking-wider w-3/4 sm:w-2/4 mx-auto my-6 px-1 py-2 sm:py-4 rounded-3xl">
         { name }
     </button>
   )
