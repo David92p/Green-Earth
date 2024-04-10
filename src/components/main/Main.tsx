8f603e71ccf6419a47e57a001ffb88d1562290c4
David92p <dave.panetta92@gmail.com>
2024-04-10 16:47:34 +0200

    fixed css

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -3,7 +3,7 @@ import {Introduction, Body} from "../index"
 
 const Main:React.FC = () => {
   return (
-    <div className='flex flex-col p-4 bg-myfirstyellow'>
+    <div className='flex flex-col bg-myfirstyellow'>
         <Introduction/>
         <Body />
     </div>
