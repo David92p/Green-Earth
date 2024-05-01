10e9aad57700b7f26d66d3a0e2309704af18513d
David92p <Dave.panetta92@gmail.com>
2024-05-01 18:27:13 +0200

    Updated status added to the toggle with relevant city data to be used for searching via latitude and longitude

--- src/components/dataSearch/DataSearch.tsx
+++ src/components/dataSearch/DataSearch.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Reasearch from './Reasearch'
+import { Button } from '../index'
+
+export type cityType = {
+  id: number
+  name: string
+  country: string
+  state: string
+  lat: number 
+  lon: number
+}
 
 const DataSearch:React.FC = () => {
+
+  const [city, setCity] = useState<cityType | null>(null)
+  const [listCity, setListCity] = useState<cityType[]>([])
+
+  // useEffect(() => {
+    //axios(`http://api.openweathermap.org/data/2.5/air_pollution?lat=39.3099931&lon=16.2501929&appid=${import.meta.env.VITE_APP_OPENWEATHER_KEY}`)
+  //   axios(`http://api.openweathermap.org/geo/1.0/direct?q=w&limit=5&appid=${import.meta.env.VITE_APP_OPENWEATHER_KEY}`)
+  //   .then(res => {
+  //     console.log(res.data)
+  //   })
+  // }, [])
+
+  const getListCity = (par:string) => {
+    // Api ricerca lista type cities con relativa latitudine e longitudine
+    axios(`http://api.openweathermap.org/geo/1.0/direct?q=${par}&limit=5&appid=${import.meta.env.VITE_APP_OPENWEATHER_KEY}`)
+    .then((res) => {
+      console.log(res.data)
+      setListCity(
+        res.data.map((result:cityType) => {
+          const { name, country, state="", lat, lon } = result
+          return { id: lat, name, country, state, lat, lon } 
+        })
+      )
+    })
+  }
+
+  useEffect(() => {
+    console.log(city)
+  }, [city])
+
+
   return (
-    <div className='bg-red-50 h-44 border-4 border-mysecondyellow' id='chart'>
-      GRAPHIC DATA INSERTED HERE
+    <div className='container-data-search' id='chart'>
+      <div className='bg-red-50 w-full h-16'>
+        Graphic inserted here
+      </div>
+      <Reasearch getListCity={getListCity} listCity={listCity} setListCity={setListCity} setCity={setCity}/>
+      <Button name='Search'/>
     </div>
   )
 }
