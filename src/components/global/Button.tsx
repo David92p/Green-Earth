403ec5cfeb8c9b8e53fa45435f026b4c137be518
David92p <dave.panetta92@gmail.com>
2024-05-10 16:39:48 +0200

    Added fnc click as an optional button parameter

--- src/components/global/Button.tsx
+++ src/components/global/Button.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 type ButtonType = {
     name: string
+    getPollutionData?: (lat: number, lon: number) => Promise<void>
+    lat?: number
+    lon?: number
 }
 
-const Button:React.FC<ButtonType> = ({ name }) => {
+const Button:React.FC<ButtonType> = ({ name, getPollutionData, lat=0, lon=0 }) => {
   return (
-    <button className="bg-mysecondyellow text-mygreen font-bold text-md sm:text-2xl tracking-wider w-3/4 sm:w-2/4 mx-auto my-6 px-1 py-2 sm:py-4 rounded-3xl">
-        { name }
+    <button onClick={() => getPollutionData ? getPollutionData(lat, lon) : null} className="bg-mysecondyellow text-mygreen font-bold text-md sm:text-2xl tracking-wider w-3/4 sm:w-2/4 mx-auto my-6 px-1 py-2 sm:py-4 rounded-3xl">
+      { name }
     </button>
   )
 }
