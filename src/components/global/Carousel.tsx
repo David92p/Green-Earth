a02bdcfc32bb8dcf0fefa80418d0d59058bfd501
David92p <dave.panetta92@gmail.com>
2024-05-22 15:37:08 +0200

    Added arrows style to Carousel

--- src/components/global/Carousel.tsx
+++ src/components/global/Carousel.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Slider from "react-slick";
 
+
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import "./carousel.css"
 
 import type  { CardType } from "./Card";
 import Card from "./Card";
@@ -12,11 +14,25 @@ export type CardsCarousel = {
   cards: CardType[];
 }
 
+const  Arrow = (props: { className: string; onClick: React.MouseEventHandler<HTMLDivElement> | undefined }) => {
+  const { className, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ display: "block", padding: "6px 4px",  background: "#FFCC31", color: "red",  height: "30px", width:"30px", borderRadius: 100, }}
+      onClick={onClick}
+    />
+  );
+}
+
 const Carousel: React.FC<CardsCarousel> = ({ cards }) => {
   const settings = {
+    infinite: true, 
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    nextArrow: <Arrow className={""} onClick={undefined} />,
+    prevArrow: <Arrow className={""} onClick={undefined} />,
     responsive: [
       {
         breakpoint: 1200,
@@ -36,7 +52,7 @@ const Carousel: React.FC<CardsCarousel> = ({ cards }) => {
   };
 
   return (  
-    <div>
+    <div className="w-[90%] m-auto">
       <Slider {...settings}>
         {
           cards.map((card) => {
