8bfa51894c844ea5ffae751e7fb2fd1792141540
David92p <dave.panetta92@gmail.com>
2024-05-22 10:14:57 +0200

    Added CSS classes to style files

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,22 @@
 import { faFacebook, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+// style 
+import "./footer.css"
 
 const Footer:React.FC = () => {
   return (
-    <div className='bg-myfirstyellow font-mytitlefont pb-2'>
-      <div className='flex flex-col justify-center items-center gap-2 h-3/5'>
-        <p className='text-mygreen text-xl sm:text-2xl font-bold tracking-wider text-center'>Follow Me</p>
-        <div className='flex justify-center gap-8'>
-          <FontAwesomeIcon icon={faLinkedin} style={{color: "#0e76a8",}} className='text-2xl sm:text-3xl'/>
-          <FontAwesomeIcon icon={faGithub} style={{color: "#171515",}} className='text-2xl sm:text-3xl'/>
-          <FontAwesomeIcon icon={faFacebook} style={{color: "#3b5998",}} className='text-2xl sm:text-3xl'/>
+    <div className='container-footer'>
+      <div className='footer-links'>
+        <p>Follow Me</p>
+        <div>
+          <FontAwesomeIcon icon={faLinkedin} style={{color: "#0e76a8",}} className='icon'/>
+          <FontAwesomeIcon icon={faGithub} style={{color: "#171515",}} className='icon'/>
+          <FontAwesomeIcon icon={faFacebook} style={{color: "#3b5998",}} className='icon'/>
         </div>
       </div>
-      <div className='flex flex-col justify-center h-2/5'>
-        <p className='text-mygreen text-md font-bold tracking-wider text-center'>© 2024 Green Earth. All rights reserved.</p>
-        <p className='text-mygreen text-lg font-bold tracking-wider text-center'>Created by Davide Panetta.</p>
+      <div className='footer-text'>
+        <p className='text-md'>© 2024 Green Earth. All rights reserved.</p>
+        <p className='text-lg'>Created by Davide Panetta.</p>
       </div>
     </div>
   )
