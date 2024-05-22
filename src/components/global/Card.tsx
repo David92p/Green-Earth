95bc17a46cd24262c6eb6e15826d5ef6c6b5d9da
David92p <dave.panetta92@gmail.com>
2024-05-22 15:36:32 +0200

    Card structure changed

--- src/components/global/Card.tsx
+++ src/components/global/Card.tsx
@@ -3,7 +3,6 @@ import React from 'react'
 import "./card.css"
 
 export type CardType = {
-		key: number
     title: string
     description: string
     img: string
@@ -11,14 +10,15 @@ export type CardType = {
 
 const Card:React.FC<CardType> = ({title, description, img}) => {
   return (
-    <div className='container-card'>
-      <div className='card-top'>
-				<img src={img} alt={title} className='card-img'/>
-			</div>
-      <div className='card-main'>
-				<p className='card-title-main'>{title}</p>
-				<p className='card-main-text text-md'>{description}</p>
-			</div>
+    <div>
+      <div className="card-top">
+        <img src={img} alt={title} />
+      </div>
+      <div className="card-main">
+        <p>{title}</p>
+        <div className='text-md'>{description}</div>
+        <a>Read More</a>
+      </div>
     </div>
   )
 }
