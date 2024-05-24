397f187936ae6f21983d327ef64e6278977525a5
David92p <Dave.panetta92@gmail.com>
2024-05-24 19:01:01 +0200

    Added external resources

--- src/components/global/Card.tsx
+++ src/components/global/Card.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // style 
 import "./card.css"
 
@@ -9,7 +9,41 @@ export type CardType = {
   img: string
 }
 
-const Card:React.FC<CardType> = ({title, description, img}) => {
+const Card:React.FC<CardType> = ({ title, description, img}) => {
+
+  const [href, setHref] = useState<string>("")
+
+  useEffect(() => {
+    switch(title) {
+      case "Carbon Monoxide":
+        setHref("https://en.wikipedia.org/wiki/Carbon_monoxide")
+        break
+      case "Nitrogen Monoxide":
+        setHref("https://en.wikipedia.org/wiki/Nitric_oxide")
+        break
+      case "Nitrogen Dioxide":
+        setHref("https://en.wikipedia.org/wiki/Nitrogen_dioxide")
+        break
+      case "Ozone":
+        setHref("https://en.wikipedia.org/wiki/Ozone")
+        break
+      case "Sulfur Dioxide":
+        setHref("https://en.wikipedia.org/wiki/Sulfur_dioxide")
+        break
+      case "Particles <PM2.5":
+        setHref("https://ww2.arb.ca.gov/resources/inhalable-particulate-matter-and-health")
+        break
+      case "Particles <PM10":
+        setHref("https://ww2.arb.ca.gov/resources/inhalable-particulate-matter-and-health")
+        break
+      case "Ammonia":
+        setHref("https://en.wikipedia.org/wiki/Ammonia")
+        break
+      default: 
+        break
+    }
+  }, [title])
+
   return (
     <div>
       <div className="card-top">
@@ -19,7 +53,10 @@ const Card:React.FC<CardType> = ({title, description, img}) => {
         <p>{title}</p>
         <div className='text text-sm sm:text-md 2xl:text-xl'>{description}</div>
         <div className='footer'>
-          <a>Read More</a>
+          <a
+            href={href}
+            target="_blank"
+          >Read More</a>
         </div>
       </div>
     </div>
