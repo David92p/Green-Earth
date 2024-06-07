f802f07171721a6ea54785fdff179ce6c587c999
David92p <dave.panetta92@gmail.com>
2024-06-07 16:52:56 +0200

    added chart specification parameter, updated option and data based on the type of chart requested by the city

--- src/components/dataSearch/Chart.tsx
+++ src/components/dataSearch/Chart.tsx
@@ -10,36 +10,42 @@ import {
     SubTitle,
     Tooltip,
     Legend,
+    PointElement,
+    LineElement,
   } from 'chart.js';
   import { Bar } from 'react-chartjs-2';
-	import type { pollutingValuesType } from './DataSearch';
-  
+  import { Line } from 'react-chartjs-2';
+	import type { pollutingValuesType, temporalAnalysisType } from './DataSearch';
   
   ChartJS.register(
     CategoryScale,
     LinearScale,
     BarElement,
+    PointElement,
+    LineElement,
     Title,
     SubTitle,
     Tooltip,
-    Legend
+    Legend,
   );
 
 type ChartType = {
-	name?: string
-	time?: string
-  quality?: string
-	pollutingValues?: pollutingValuesType
+  type: "current" | "historical"
+	name: string
+	time: string[]
+  quality: string[]
+	pollutingValues: pollutingValuesType
+  temporalAnalysis?: temporalAnalysisType
 }  
 
-const Chart:React.FC<ChartType> = ({ name, time, quality, pollutingValues }) => {
+const Chart:React.FC<ChartType> = ({ type, name, time, quality, pollutingValues, temporalAnalysis }) => {
 
   const [largeScreen] = useState(window.innerWidth)
 
-  const labels = ['co', 'no', 'no2', 'o3', 'so2', 'pm2_5', 'pm10', 'nh3'];
-
+  const labels = type == "current" ? ['co', 'no', 'no2', 'o3', 'so2', 'pm2.5', 'pm10', 'nh3'] : time
+ 
   const options = {
-    indexAxis: largeScreen < 641 ? "y" as const : 'x' as const,
+    indexAxis: type == "current" &&  largeScreen < 641 ? "y" as const : 'x' as const,
     elements: {
 			bar: {
 				borderWidth: 2,
@@ -66,19 +72,18 @@ const Chart:React.FC<ChartType> = ({ name, time, quality, pollutingValues }) => 
         text: name,
         color: "#37818A",
         font: {
-          size: largeScreen < 641 ? 25 : (largeScreen < 1030 ? 30 : 40),
+          size: largeScreen < 641 ? 25 : (largeScreen < 1030 ? 40 : 45),
           lineHeight: 2,
         },
       },
       subtitle: {
         display: true,
-        text: [time, quality],
+        text: type == "current" ? [time[0], quality[0]] : temporalAnalysis &&  temporalAnalysis.analysisPeriod.charAt(0).toUpperCase() + temporalAnalysis.analysisPeriod.slice(1) +" analysis",
         color: "#37818A",
         font: {
           size: largeScreen < 641 ? 20 : (largeScreen < 1030 ? 30 : 40),
           lineHeight: 1.2
         },
-        
         padding: {
           bottom: 25
         }
@@ -105,7 +110,7 @@ const Chart:React.FC<ChartType> = ({ name, time, quality, pollutingValues }) => 
         },
       },
       x: {
-        display: true,
+        display: type == "historical" && largeScreen < 641 ? false : true,
         ticks: {
           color: '#37818A',
           font: {size: largeScreen < 641 ? 18 : (largeScreen < 1030 ? 22 : 30), weight: "bold" as const},
@@ -114,58 +119,106 @@ const Chart:React.FC<ChartType> = ({ name, time, quality, pollutingValues }) => 
       }
     }
   };
-    
+
   const data = {
     labels,
-    datasets: [{
-      label: 'µg/m³',
-      data: pollutingValues && Object.values(pollutingValues),
-      borderColor: [
-				'rgba(255, 99, 132, 0.4)',
-        'rgba(255, 159, 64, 0.4)',
-        'rgba(255, 205, 86, 0.4)',
-        'rgba(75, 192, 192, 0.4)',
-        'rgba(54, 162, 235, 0.4)',
-        'rgba(153, 102, 255, 0.4)',
-        'rgba(255, 99, 132, 0.4)',
-        'rgba(75, 192, 192, 0.4)',
-			],
-			hoverBorderColor: [
-        'rgba(255, 99, 132, 0.8)',
-        'rgba(255, 159, 64, 0.8)',
-        'rgba(255, 205, 86, 0.8)',
-        'rgba(75, 192, 192, 0.8)',
-        'rgba(54, 162, 235, 0.8)',
-        'rgba(153, 102, 255, 0.8)',
-        'rgba(255, 99, 132, 0.8)',
-        'rgba(75, 192, 192, 0.8)',
-      ],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.4)',
-        'rgba(255, 159, 64, 0.4)',
-        'rgba(255, 205, 86, 0.4)',
-        'rgba(75, 192, 192, 0.4)',
-        'rgba(54, 162, 235, 0.4)',
-        'rgba(153, 102, 255, 0.4)',
-        'rgba(255, 99, 132, 0.4)',
-        'rgba(75, 192, 192, 0.4)',
-      ],
-      hoverBackgroundColor: [
-        'rgba(255, 99, 132, 0.8)',
-        'rgba(255, 159, 64, 0.8)',
-        'rgba(255, 205, 86, 0.8)',
-        'rgba(75, 192, 192, 0.8)',
-        'rgba(54, 162, 235, 0.8)',
-        'rgba(153, 102, 255, 0.8)',
-        'rgba(255, 99, 132, 0.8)',
-        'rgba(75, 192, 192, 0.8)',
-      ],
-			minBarLength: 20,
-    }],
+    datasets: type == "current" ? (
+      [{
+        label: 'µg/m³',
+        data: pollutingValues && Object.values(pollutingValues[0]),
+        borderColor: [
+          'rgba(255, 99, 132, 0.4)',
+          'rgba(255, 159, 64, 0.4)',
+          'rgba(255, 205, 86, 0.4)',
+          'rgba(75, 192, 192, 0.4)',
+          'rgba(54, 162, 235, 0.4)',
+          'rgba(153, 102, 255, 0.4)',
+          'rgba(255, 99, 132, 0.4)',
+          'rgba(75, 192, 192, 0.4)',
+        ],
+        hoverBorderColor: [
+          'rgba(255, 99, 132, 0.8)',
+          'rgba(255, 159, 64, 0.8)',
+          'rgba(255, 205, 86, 0.8)',
+          'rgba(75, 192, 192, 0.8)',
+          'rgba(54, 162, 235, 0.8)',
+          'rgba(153, 102, 255, 0.8)',
+          'rgba(255, 99, 132, 0.8)',
+          'rgba(75, 192, 192, 0.8)',
+        ],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.4)',
+          'rgba(255, 159, 64, 0.4)',
+          'rgba(255, 205, 86, 0.4)',
+          'rgba(75, 192, 192, 0.4)',
+          'rgba(54, 162, 235, 0.4)',
+          'rgba(153, 102, 255, 0.4)',
+          'rgba(255, 99, 132, 0.4)',
+          'rgba(75, 192, 192, 0.4)',
+        ],
+        hoverBackgroundColor: [
+          'rgba(255, 99, 132, 0.8)',
+          'rgba(255, 159, 64, 0.8)',
+          'rgba(255, 205, 86, 0.8)',
+          'rgba(75, 192, 192, 0.8)',
+          'rgba(54, 162, 235, 0.8)',
+          'rgba(153, 102, 255, 0.8)',
+          'rgba(255, 99, 132, 0.8)',
+          'rgba(75, 192, 192, 0.8)',
+        ],
+        minBarLength: 20,
+      }]
+    ) : (
+      [{
+        label: "µg/m³",
+        data: pollutingValues && temporalAnalysis && pollutingValues?.map((element) => element[temporalAnalysis?.substance]),
+        // borderColor: [
+        //   'rgba(255, 99, 132, 0.4)',
+        //   'rgba(255, 159, 64, 0.4)',
+        //   'rgba(255, 205, 86, 0.4)',
+        //   'rgba(75, 192, 192, 0.4)',
+        //   'rgba(54, 162, 235, 0.4)',
+        //   'rgba(153, 102, 255, 0.4)',
+        //   'rgba(255, 99, 132, 0.4)',
+        //   'rgba(75, 192, 192, 0.4)',
+        // ],
+        // hoverBorderColor: [
+        //   'rgba(255, 99, 132, 0.8)',
+        //   'rgba(255, 159, 64, 0.8)',
+        //   'rgba(255, 205, 86, 0.8)',
+        //   'rgba(75, 192, 192, 0.8)',
+        //   'rgba(54, 162, 235, 0.8)',
+        //   'rgba(153, 102, 255, 0.8)',
+        //   'rgba(255, 99, 132, 0.8)',
+        //   'rgba(75, 192, 192, 0.8)',
+        // ],
+        // backgroundColor: [
+        //   'rgba(255, 99, 132, 0.4)',
+        //   'rgba(255, 159, 64, 0.4)',
+        //   'rgba(255, 205, 86, 0.4)',
+        //   'rgba(75, 192, 192, 0.4)',
+        //   'rgba(54, 162, 235, 0.4)',
+        //   'rgba(153, 102, 255, 0.4)',
+        //   'rgba(255, 99, 132, 0.4)',
+        //   'rgba(75, 192, 192, 0.4)',
+        // ],
+        // hoverBackgroundColor: [
+        //   'rgba(255, 99, 132, 0.8)',
+        //   'rgba(255, 159, 64, 0.8)',
+        //   'rgba(255, 205, 86, 0.8)',
+        //   'rgba(75, 192, 192, 0.8)',
+        //   'rgba(54, 162, 235, 0.8)',
+        //   'rgba(153, 102, 255, 0.8)',
+        //   'rgba(255, 99, 132, 0.8)',
+        //   'rgba(75, 192, 192, 0.8)',
+        // ],
+      }]
+    ) 
   };
 
-  return <Bar options={options} data={data} className='py-6 px-2 sm:px-6 2xl:px-10'/>
-  
+  return (
+    type == "historical" ? <Line options={options} data={data} className='p-2 sm:px-6 2xl:px-10'/> : <Bar options={options} data={data} className='p-2 sm:px-6 2xl:px-10'/>
+  )
 }
 
 export default Chart
