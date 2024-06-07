6f54195b075bfa3602472eb185a980f53670ecf2
David92p <dave.panetta92@gmail.com>
2024-06-07 16:48:06 +0200

    Data call fnc name changed

--- src/components/global/Button.tsx
+++ src/components/global/Button.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
 // style 
 import "./button.css"
+// type 
+import { cityDataType } from '..'
 
 type ButtonType = {
     name: string
-    getPollutionData?: (lat: number, lon: number) => Promise<void>
+    getCurrentPollutionData?: (lat: number, lon: number) => Promise<cityDataType | null | void>
     lat?: number
     lon?: number
 }
 
-const Button:React.FC<ButtonType> = ({ name, getPollutionData, lat=0, lon=0 }) => {
+const Button:React.FC<ButtonType> = ({ name, getCurrentPollutionData, lat=0, lon=0 }) => {
   return (
-    <button onClick={() => getPollutionData ? getPollutionData(lat, lon) : null} className='container-btn text-md sm:text-2xl' >
+    <button onClick={() => getCurrentPollutionData ? getCurrentPollutionData(lat, lon) : null} className='container-btn text-md sm:text-2xl' >
       { name }
     </button>
   )
