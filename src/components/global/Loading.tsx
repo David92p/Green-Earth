59c99f6cc5b7ae008d2bd305120508c28207519f
David92p <Dave.panetta92@gmail.com>
2024-06-09 12:38:45 +0200

    Centered loading to container

--- src/components/global/Loading.tsx
+++ src/components/global/Loading.tsx
@@ -3,7 +3,7 @@ import loading from "../../assets/images/loading.gif"
 
 const Loading:React.FC = () => {
   return (
-    <div className='flex justify-center items-center h-[300px] w-full'>
+    <div className='flex justify-center items-center h-full w-full'>
 			<img src={loading} alt="Loading ..." className=''/>
     </div>
   )
