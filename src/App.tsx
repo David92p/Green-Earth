068ba20de96e89ecadfde696456dcacd0b2fe366
David92p <dave.panetta92@gmail.com>
2024-06-12 09:03:40 +0200

    Removed unused code

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,6 @@ import "./App.css"
 
 const App:React.FC = () => {
 
-  // useEffect(() => {
-  //   fetch("https://api.v2.emissions-api.org/api/v2/carbonmonoxide/statistics.json?country=it&interval=week&end=2024-04-05&limit=1&offset=0")
-  //   .then(res => {
-  //     const x = res.json()
-  //     return x
-  //   })
-  //   .then(res => {
-  //     console.log(res)
-  //   })
-  // }, [])
-
   return (
     <div className="container">
       <Navbar />
