15e2563b320fdeb46eafe5075f3eaa7a69f3c392
David92p <dave.panetta92@gmail.com>
2024-06-17 14:03:20 +0200

    Added navigate to button

--- src/components/global/Button.tsx
+++ src/components/global/Button.tsx
@@ -3,17 +3,21 @@ import React from 'react'
 import "./button.css"
 // type 
 import { cityDataType } from '..'
+import { useNavigate } from 'react-router-dom'
 
 type ButtonType = {
     name: string
     getCurrentPollutionData?: (lat: number, lon: number) => Promise<cityDataType | null | void>
+    loadPage?: string
     lat?: number
     lon?: number
 }
 
-const Button:React.FC<ButtonType> = ({ name, getCurrentPollutionData, lat=0, lon=0 }) => {
+const Button:React.FC<ButtonType> = ({ name, getCurrentPollutionData, loadPage, lat=0, lon=0 }) => {
+  const navigate = useNavigate()
+  
   return (
-    <button onClick={() => getCurrentPollutionData ? getCurrentPollutionData(lat, lon) : null} className='container-btn text-md sm:text-2xl' >
+    <button onClick={() => getCurrentPollutionData ? getCurrentPollutionData(lat, lon) : loadPage && navigate(loadPage)} className='container-btn text-md sm:text-2xl' >
       { name }
     </button>
   )
