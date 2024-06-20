2ad0bb59b78459146656ddd0686b2799b6913d6e
David92p <dave.panetta92@gmail.com>
2024-06-20 12:09:17 +0200

    added loading type for the contact form

--- src/components/global/Pending.tsx
+++ src/components/global/Pending.tsx
@@ -7,13 +7,13 @@ import error from "../../assets/images/error.png"
 import "./pending.css"
 
 type PendingType = {
-  type: "loading" | "error"
+  type: "loading" | "loading-form" | "error"
 }
 
 const Pending:React.FC<PendingType> = ({ type }) => {
   return (
     <div className='container-pending'>
-			<img src={type == "loading" ? loading : error} alt={type == "loading" ? "Loading ..." : "ERROR 404"}/>
+			<img src={type == "loading" || type == "loading-form" ? loading : error} alt={type == "loading" ? "Loading ..." : type == "loading-form" ? "Sending...." : "ERROR 404"}/>
     </div>
   )
 }
